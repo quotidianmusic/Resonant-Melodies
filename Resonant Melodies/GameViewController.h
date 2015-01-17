ebe9fe12c497b023a2b1f03a59467aa43acb78e9
Justin Lennox <quotidianmusic@gmail.com>
2015-01-17 01:43:25 -0500

    Initial Commit

---
+++ Resonant Melodies/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  Resonant Melodies
+//
+
+//  Copyright (c) 2015 Justin Lennox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
