3a2011f29007a4fe4b01aa8c47459088bef47f29
Justin Lennox <quotidianmusic@gmail.com>
2015-02-10 04:48:15 -0500

    Intro Added, Working Level 1

--- Resonant Melodies/GameViewController.m
+++ Resonant Melodies/GameViewController.m
@@ -7,8 +7,51 @@
 //
 
 #import "GameViewController.h"
+@import CoreMotion;
+#import <CoreData/CoreData.h>
 
-@implementation GameViewController
+
+@implementation GameViewController{
+    SKSpriteNode *_ship;    //1
+}
+
+-(id)initWithSize:(CGSize)size {
+    if (self = [super initWithSize:size]) {
+        /* Setup your scene here */
+        //2
+        NSLog(@"SKScene:initWithSize %f x %f",size.width,size.height);
+        
+        //3
+        self.backgroundColor = [SKColor blackColor];
+        
+#pragma mark - TBD - Game Backgrounds
+        
+#pragma mark - Setup Sprite for the ship
+        //Create space sprite, setup position on left edge centered on the screen, and add to Scene
+        //4
+        _ship = [SKSpriteNode spriteNodeWithImageNamed:@"SpaceFlier_sm_1.png"];
+        _ship.position = CGPointMake(self.frame.size.width * 0.1, CGRectGetMidY(self.frame));
+        [self addChild:_ship];
+        
+#pragma mark - TBD - Setup the asteroids
+        
+#pragma mark - TBD - Setup the lasers
+        
+#pragma mark - TBD - Setup the Accelerometer to move the ship
+        
+#pragma mark - TBD - Setup the stars to appear as particles
+        
+#pragma mark - TBD - Start the actual game
+        
+    }
+    return self;
+}
+
+
+-(void)update:(NSTimeInterval)currentTime {
+    /* Called before each frame is rendered */
+}
+
 
 - (void)viewDidLoad
 {
